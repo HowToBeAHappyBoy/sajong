704cbf19b2d442f4d2c24fd1ee5c07da95697f56
max.cha <max.cha@kakaocorp.com>
2022-12-03 14:22:28 +0900

    feat: 데이터 description 추가

--- frontend/src/msw/data.ts
+++ frontend/src/msw/data.ts
@@ -3,6 +3,7 @@ type WishItem = {
   title: string;
   image: string;
   link: string;
+  description: string;
 };
 
 export const MOCK_WISH_ITEMS: WishItem[] = [
@@ -12,83 +13,97 @@ export const MOCK_WISH_ITEMS: WishItem[] = [
     image:
       'https://newsimg.hankookilbo.com/cms/articlerelease/2020/11/01/5a4b5f41-c8bb-447c-9dc6-0263674670bd.jpg',
     link: 'http://thehill894.com/',
+    description: '사주세요. 제발',
   },
   {
     id: 1,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 2,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 3,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 4,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 5,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 6,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 7,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 8,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 9,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 10,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 11,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 12,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
   {
     id: 13,
     title: '람보르기니',
     image: 'https://www.motorgraph.com/news/photo/202110/28487_90831_4424.jpg',
     link: 'https://auto.daum.net/newcar/brand/bo80000g00us',
+    description: '사주세요. 제발',
   },
 ];
