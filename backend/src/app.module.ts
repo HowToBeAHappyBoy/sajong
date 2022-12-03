4469c611702746492a689827897313c4d50bc63a
n4oah <n4oahdev@gmail.com>
2022-12-03 14:56:17 +0900

    chore: 불필요한 코드 제거

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './apis/user/user.module';
 import { WishListModule } from './apis/wish-list/wish-list.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -15,13 +14,11 @@ import { AppService } from './app.service';
       password: 'sajong12',
       database: 'sajong',
       entities: ['dist/**/*.entity{.ts,.js}'],
-      migrations: ['dist/migrations/*{.ts,.js}'],
       synchronize: true,
     }),
     UserModule,
     WishListModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
